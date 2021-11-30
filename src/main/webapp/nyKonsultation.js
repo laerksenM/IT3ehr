b824610f8f9bd611c0372ada02634a00123cd355
miafjensen <78786706+miafjensen@users.noreply.github.com>
2021-11-30 14:09:42 +0100

    virker ikke endnu

--- src/main/webapp/nyKonsultation.js
+++ src/main/webapp/nyKonsultation.js
@@ -1,11 +1,30 @@
-async function opretNyKonsultation(){
-    let result = await fetch("rest/konsultationer");
+async function opretNyKonsultation() {
+    let form = document.getElementById("konsultationForm");
+    let formKonsul = new FormData(form);
+    let aftaleJson = Object.fromEntries(formKonsul)
+    console.log(aftaleJson);
+    let res = await fetch("rest/opretKonsultation", {
+            method: "POST",
+            body: JSON.stringify(aftaleJson),
+            headers: {
+                "content-type": "application/json",
+                "authorization": localStorage.getItem("token") //vi getter authoration fra hvor vores token er gemt
+            }
+        }
+    );
+    console.log(res.body);
+    console.log(res.status)
+    if (res.status != 200) {
+        alert("noget gik galt: " + res.status)
 
-    console.log(result.status)
-    if (result.status!=200){
-        alert("noget gik galt!");
+    } else {
+        console.log(res.status)
+        console.log(res.status + "det lykkedes");
+        window.location.href = "kalender.html"
     }
-    let json = await result.json();
+
+    let json = await res.json();
     console.log(json)
-    // Ovenstående kode ikke færdig!
+
+// Ovenstående kode ikke færdig!
 }
